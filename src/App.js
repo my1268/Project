48104937064fc1664c809e267bc2b62d3d8fe890
yeonsuBaek <soothbugger@gmail.com>
2023-07-11 14:56:08 +0900

    style: Pagination UI 컴포넌트 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,38 @@
+import Ghost from './UI/Button/Ghost';
+import Kakao from './UI/Button/Kakao';
+import Mini from './UI/Button/Mini';
+import Primary from './UI/Button/Primary';
+import Base from './UI/Form/Base';
+import ButtonType from './UI/Form/ButtonType';
+import Date from './UI/Form/Date';
+import LoginType from './UI/Form/LoginType';
+import Memo from './UI/Form/Memo';
+import Pagination from './UI/Pagination/Pagination';
+
 function App() {
-  return <div>hello</div>;
+  return (
+    <div>
+      <div>
+        <Primary text='버튼' isShortPrimary={false} />
+        <Primary text='버튼' isShortPrimary={true} />
+        <Mini text='버튼' color='gray' />
+        <Mini text='버튼' color='red' />
+        <Ghost text='버튼' isOpenCategory={false} />
+        <Ghost text='버튼' isOpenCategory={true} />
+        <Kakao />
+      </div>
+      <div>
+        <Base placeholder='텍스트를 입력하세요' />
+        <ButtonType placeholder='이메일' buttonText='중복확인' />
+        <LoginType />
+        <Date />
+        <Memo placeholder='메모를 입력하세요' />
+      </div>
+      <div>
+        <Pagination />
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/UI/Pagination/PageButton.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function PageButton({ number }) {
+  return <button type='button'>{number}</button>;
+}
+
+export default PageButton;

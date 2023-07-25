74d7b5cbdb841f43f6aef8c1b198f0513387810e
YeonsuBaek <soothbugger@gmail.com>
2023-07-25 19:30:28 +0900

    feat: MyMenu Page 생성

---
+++ src/components/pages/MyMenu.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import Categories from '../Categories';
+import PageCover from '../PageCover';
+import myMenu from './MyMenu.module.css';
+import Base from '../../UI/Form/Base';
+import Primary from '../../UI/Button/Primary';
+
+function MyMenu() {
+  return (
+    <>
+      <PageCover title='마이페이지' />
+      <div className={myMenu.layout}>
+        <Categories />
+
+        <div className={myMenu.container}>
+          <form className={myMenu.form}>
+            <dl className={myMenu.list}>
+              <div className={myMenu.item}>
+                <dt>이메일</dt>
+                <dd>1234@gmail.com</dd>
+              </div>
+              <div className={myMenu.item}>
+                <dt>이름</dt>
+                <dd>
+                  <Base placeholder='홍길동' />
+                </dd>
+              </div>
+              <div className={myMenu.item}>
+                <dt>닉네임</dt>
+                <dd>
+                  {' '}
+                  <Base placeholder='고길동' />
+                </dd>
+              </div>
+            </dl>
+            <Primary text='회원 정보 수정' />
+          </form>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default MyMenu;

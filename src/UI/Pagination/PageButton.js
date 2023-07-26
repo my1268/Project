64df2c498023d8fa4af9a74a64791940eb46f487
YeonsuBaek <soothbugger@gmail.com>
2023-07-26 20:44:52 +0900

    fix: PageButton 색상 지정

--- src/UI/Pagination/PageButton.js
+++ src/UI/Pagination/PageButton.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import page from './Pagination.module.css';
 
 function PageButton({ number }) {
-  return <button type='button'>{number}</button>;
+  return (
+    <button className={page.button} type='button'>
+      {number}
+    </button>
+  );
 }
 
 export default PageButton;

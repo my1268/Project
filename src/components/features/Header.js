715fedde4df3bc7dc120b88460afd28a92e54cfc
TW <my1268@naver.com>
2023-07-28 14:44:12 +0900

    refactor: features 폴더 생성 및 파일 이동

--- src/components/features/Header.js
+++ src/components/features/Header.js
@@ -1,29 +1,31 @@
-import React, { useState } from 'react';
-import header from './Header.module.css';
-import { AiOutlineMenu } from 'react-icons/ai';
-import Ghost from '../../UI/Button/Ghost';
-import Primary from '../../UI/Button/Primary';
-import Menu from '../../UI/Dropdown/Menu';
-import { BsFillPersonFill } from 'react-icons/bs';
-import LoginModal from '../../UI/Modal/LoginModal';
-import Overlay from '../../UI/Modal/Overlay';
+import React, { useState } from "react";
+import header from "./Header.module.css";
+import { AiOutlineMenu } from "react-icons/ai";
+import Ghost from "../../UI/Button/Ghost";
+import Primary from "../../UI/Button/Primary";
+import Menu from "../../UI/Dropdown/Menu";
+import { BsFillPersonFill } from "react-icons/bs";
+import LoginModal from "../../UI/Modal/LoginModal";
+import Overlay from "../../UI/Modal/Overlay";
+import SignUpModal from "../../UI/Modal/SignUpModal";
 
 function Header() {
   const [isOpenLogin, setIsOpenLogin] = useState(false);
+  const [isOpenSignUp, setIsOpenSignUp] = useState(false);
   return (
     <div className={header.wrapper}>
       <header className={header.header}>
         <h1 className={header.logo}>
-          <a href='/'>Trip Planner</a>
+          <a href="/">Trip Planner</a>
         </h1>
         <div className={`sm-only ${header.buttonList}`}>
-          <button className={header.menu} type='button'>
+          <button className={header.menu} type="button">
             <AiOutlineMenu />
           </button>
           {/* 비회원인 경우 */}
           <Ghost
-            text='로그인'
-            style={{ color: '#3da5f5' }}
+            text="로그인"
+            style={{ color: "#3da5f5" }}
             onClick={() => setIsOpenLogin(true)}
           />
           {/* 회원인 경우 */}
@@ -33,18 +35,28 @@ function Header() {
         </div>
         <div className={`lg-only ${header.menuList}`}>
           <div className={header.categoy}>
-            <Ghost text='플래너 작성' />
-            <Ghost text='리뷰 게시판' />
+            <Ghost text="플래너 작성" />
+            <Ghost text="리뷰 게시판" />
           </div>
           {/* 비회원인 경우 */}
           <div className={header.member}>
             <Ghost
-              text='로그인'
-              style={{ color: '#3da5f5' }}
+              text="로그인"
+              style={{ color: "#3da5f5" }}
               onClick={() => setIsOpenLogin(true)}
             />
-            <a href='/'>
-              <Primary isShortPrimary='true' text='회원가입' />
+            <a
+              href="/"
+              onClick={(e) => {
+                e.preventDefault();
+                setIsOpenSignUp(true);
+              }}
+            >
+              <Primary
+                isShortPrimary="true"
+                text="회원가입"
+                onClick={() => setIsOpenSignUp(true)}
+              />
             </a>
           </div>
           {/* 회원인 경우 */}
@@ -53,11 +65,17 @@ function Header() {
           </a> */}
         </div>
       </header>
-      <Menu isOpenMenu='false' />
+      <Menu isOpenMenu="false" />
       {isOpenLogin && (
         <>
           <LoginModal onClick={() => setIsOpenLogin(false)} />
           <Overlay onClick={() => setIsOpenLogin(false)} />
+        </>
+      )}
+      {isOpenSignUp && ( // 회원가입 모달 렌더링
+        <>
+          <SignUpModal onClick={() => setIsOpenSignUp(false)} />
+          <Overlay onClick={() => setIsOpenSignUp(false)} />
         </>
       )}
     </div>

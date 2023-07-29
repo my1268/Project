7e9314ec15d16eb5e19d6d5d3156248fd9361eb6
YeonsuBaek <soothbugger@gmail.com>
2023-07-30 00:04:26 +0900

    style: PlannerModal 컴포넌트 생성

---
+++ src/UI/Modal/PlannerModal.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import Ghost from '../Button/Ghost';
+import modal from './Modal.module.css';
+import { GrClose } from 'react-icons/gr';
+import { AiTwotoneDelete } from 'react-icons/ai';
+import { MdEdit } from 'react-icons/md';
+import CardList from '../Card/CardList';
+import demoImage from '../../assets/images/놀이공원.png';
+import TimeTable from '../TimeTable/TimeTable';
+
+function PlannerModal() {
+  const placeList = [
+    {
+      name: '놀이공원1',
+      date: '3월 10일 12:00 - 13:00',
+      address: '서울특별시 강남구',
+      image: demoImage,
+    },
+    {
+      name: '놀이공원2',
+      date: '3월 10일 12:00 - 13:00',
+      address: '서울특별시 강남구',
+      image: demoImage,
+    },
+    {
+      name: '놀이공원3',
+      date: '3월 10일 12:00 - 13:00',
+      address: '서울특별시 강남구',
+      image: demoImage,
+    },
+  ];
+  return (
+    <aside className={`${modal.base} ${modal.posting}`}>
+      <header className={modal.header}>
+        <h2>플래너 제목</h2>
+        <div className={modal.buttonWrapper}>
+          <Ghost text='수정' style={{ color: '#3DA5F5' }} className='lg-only' />
+          <Ghost text='삭제' style={{ color: '#F86D7D' }} className='lg-only' />
+          <button type='button' className={`sm-only ${modal.edit}`}>
+            <MdEdit />
+          </button>
+          <button type='button' className={`sm-only ${modal.delete}`}>
+            <AiTwotoneDelete />
+          </button>
+        </div>
+        <button type='button' className={`sm-only ${modal.close}`}>
+          <GrClose />
+        </button>
+      </header>
+      <div className={modal.section}>
+        <h3>타임테이블</h3>
+        <TimeTable />
+      </div>
+      <div className={modal.section}>
+        <h3>장소 정보</h3>
+        <CardList placeList={placeList} />
+      </div>
+    </aside>
+  );
+}
+
+export default PlannerModal;

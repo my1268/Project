00b4250b97a5337d285e4f769427850f32642e10
TW <my1268@naver.com>
2023-08-01 21:14:43 +0900

    link태그 교체

--- src/components/features/Categories.js
+++ src/components/features/Categories.js
@@ -1,23 +1,38 @@
-import React from 'react';
-import categories from './Categories.module.css';
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+import categories from "./Categories.module.css";
 
 function Categories() {
+  const location = useLocation();
+
   return (
     <ul className={categories.categories}>
-      <li className={categories.isOpen}>
-        <a href='/'>내 정보</a>
+      <li>
+        <Link
+          to="/mymenu"
+          className={location.pathname === "/mymenu" ? categories.isOpen : null}
+        >
+          내 정보
+        </Link>
       </li>
       <li>
-        <a href='/'>비밀번호 수정</a>
+        <Link to="/">비밀번호 수정</Link>
       </li>
       <li>
-        <a href='/'>내 플래너</a>
+        <Link
+          to="/myplanner"
+          className={
+            location.pathname === "/myplanner" ? categories.isOpen : null
+          }
+        >
+          내 플래너
+        </Link>
       </li>
       <li>
-        <a href='/'>내 게시글</a>
+        <Link to="/">내 게시글</Link>
       </li>
       <li>
-        <a href='/'>내 댓글</a>
+        <Link to="/">내 댓글</Link>
       </li>
     </ul>
   );

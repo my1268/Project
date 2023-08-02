6cbf3d60abb5126dd62aa413cdfd434acaf92397
TW <my1268@naver.com>
2023-08-02 18:32:24 +0900

    Router추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
 import Header from "./components/features/Header";
 import Main from "./components/pages/Main";
-import MakingPlanner from "./components/pages/MakingPlanner";
 import MyMenu from "./components/pages/MyMenu";
 import MyPlanner from "./components/pages/MyPlanner";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import MyPasswordUpdate from "./components/pages/MyPasswordUpdate";
 
 function App() {
   return (
@@ -13,8 +13,8 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Main />} />
-          <Route path="/makingplanner" element={<MakingPlanner />} />
           <Route path="/mymenu" element={<MyMenu />} />
+          <Route path="/mypasswordupdate" element={<MyPasswordUpdate />} />
           <Route path="/myplanner" element={<MyPlanner />} />
         </Routes>
       </>

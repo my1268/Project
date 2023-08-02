2e175528fe3c93af8e1d19db639f56e0af7b8efd
TW <my1268@naver.com>
2023-08-02 18:41:11 +0900

    onchange, value 추가

--- src/UI/Form/ButtonType.js
+++ src/UI/Form/ButtonType.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import form from './Form.module.css';
-import Mini from '../Button/Mini';
+import React from "react";
+import form from "./Form.module.css";
+import Mini from "../Button/Mini";
 
-function ButtonType({ placeholder, buttonText }) {
+function ButtonType({ placeholder, buttonText, onChange, value }) {
   return (
     <div className={form.buttonType}>
-      <input className={form.base} type='text' placeholder={placeholder} />
-      <Mini text='중복확인' color='gray' />
+      <input
+        className={form.base}
+        type="text"
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+      />
+      <Mini text="중복확인" color="gray" />
     </div>
   );
 }

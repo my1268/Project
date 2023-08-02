d12e43e427cb6cfc50c7168b4ded75ddf26a95ad
TW <my1268@naver.com>
2023-08-02 18:52:44 +0900

    style: SignUp 컴포넌트 생성

--- src/UI/Modal/SignUpModal.js
+++ src/UI/Modal/SignUpModal.js
@@ -1,7 +1,6 @@
 import React from "react";
 import modal from "./Modal.module.css";
 import SignUpType from "../Form/SignUpType";
-import Primary from "../Button/Primary";
 import { GrClose } from "react-icons/gr";
 
 function SignUpModal({ onClick }) {
@@ -9,7 +8,6 @@ function SignUpModal({ onClick }) {
     <aside className={`${modal.base} ${modal.member}`}>
       <h2>회원가입</h2>
       <SignUpType style={{ marginBottom: "16px" }} />
-      <Primary text="회원가입" style={{ marginBottom: "64px" }} />
       <div className={modal.extraButtons}></div>
       <button
         type="button"

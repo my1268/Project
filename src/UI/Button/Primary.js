8f26330255e4a52c3a5178a49799d552990da76a
TW <my1268@naver.com>
2023-08-02 18:36:59 +0900

    onclick추가

--- src/UI/Button/Primary.js
+++ src/UI/Button/Primary.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import button from './Button.module.css';
+import React from "react";
+import button from "./Button.module.css";
 
-function Primary({ text, isShortPrimary, style }) {
+function Primary({ text, isShortPrimary, style, onClick }) {
   return (
     <button
       className={`${button.base} ${button.primary} ${
         isShortPrimary && button.short
       }`}
       style={style}
-      type='button'
+      type="button"
+      onClick={onClick}
     >
       {text}
     </button>

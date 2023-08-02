ae84dc80a73fc0e11b86b79d84483b34c412ee62
TW <my1268@naver.com>
2023-08-02 18:41:42 +0900

    placeholder 수정

--- src/UI/Form/LoginType.js
+++ src/UI/Form/LoginType.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import form from './Form.module.css';
+import React from "react";
+import form from "./Form.module.css";
 
 function LoginType({ style }) {
   return (
     <div style={style} className={form.loginType}>
-      <input className={form.base} type='text' placeholder='아이디' />
-      <input className={form.base} type='password' placeholder='비밀번호' />
+      <input className={form.base} type="text" placeholder="이메일" />
+      <input className={form.base} type="password" placeholder="비밀번호" />
     </div>
   );
 }

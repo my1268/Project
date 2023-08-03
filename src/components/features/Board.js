6114003679c18d0f1d3cd68504ed1763d26b3ac8
TW <my1268@naver.com>
2023-08-03 21:22:46 +0900

    title추가

--- src/components/features/Board.js
+++ src/components/features/Board.js
@@ -1,23 +1,23 @@
-import React from 'react';
-import board from './Board.module.css';
-import Mini from '../../UI/Button/Mini';
+import React from "react";
+import board from "./Board.module.css";
+import Mini from "../../UI/Button/Mini";
 
-function Board({ list }) {
+function Board({ list, onClick, title }) {
   return (
     <ul className={board.table}>
       <li className={board.tableHeader}>
-        <h3>플래너 목록</h3>
-        <h3 className='lg-only'>날짜</h3>
-        <div className='lg-only'></div>
+        <h3>{title}</h3>
+        <h3 className="lg-only">날짜</h3>
+        <div className="lg-only"></div>
       </li>
       {list.map((item) => {
         return (
           <li className={board.tableBody} key={item.title}>
-            <a href={item.page} className={board.title}>
+            <button type="button" className={board.title} onClick={onClick}>
               {item.title}
-            </a>
+            </button>
             <span className={`lg-only ${board.date}`}>{item.date}</span>
-            <Mini color='red' text='삭제' />
+            <Mini color="red" text="삭제" />
           </li>
         );
       })}

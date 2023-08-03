e0b1339a20266248033a895f86c6341ef3d7e116
TW <my1268@naver.com>
2023-08-03 21:37:32 +0900

    link추가

--- src/UI/Dropdown/Menu.js
+++ src/UI/Dropdown/Menu.js
@@ -1,13 +1,19 @@
-import React from 'react';
-import dropdown from './Dropdown.module.css';
+import React from "react";
+import dropdown from "./Dropdown.module.css";
+import { Link } from "react-router-dom";
 
-function Menu({ isOpenMenu }) {
+function Menu({ isOpenMenu, onClick }) {
+  const handleMenuClick = () => {
+    onClick && onClick();
+  };
   return (
     <aside
       className={`sm-only ${dropdown.menu} ${isOpenMenu && dropdown.menuOpen}`}
     >
-      <a href='/'>플래너 작성</a>
-      <a href='/'>리뷰 게시판</a>
+      <Link to="/makingplanner" onClick={handleMenuClick}>
+        플래너 작성
+      </Link>
+      <a href="/">리뷰 게시판</a>
     </aside>
   );
 }

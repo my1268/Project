26ad4d6d128aec3b914c3991728d89219c94159c
TW <my1268@naver.com>
2023-08-22 16:45:17 +0900

    코드 수정

--- src/UI/Modal/PasswordModal.js
+++ src/UI/Modal/PasswordModal.js
@@ -1,22 +1,23 @@
 import React, { useState } from "react";
 import modal from "./Modal.module.css";
-import Primary from "../Button/Primary";
 import { GrClose } from "react-icons/gr";
 import PasswordType from "../Form/PasswordType";
 import NewPasswordModal from "./NewPasswordModal";
 
 function PasswordModal({ onClick }) {
   const [showNewPassword, setShowNewPassword] = useState(false);
+  const [passwordUpdated, setPasswordUpdated] = useState(false);
 
-  return (
+  const handlePasswordUpdated = () => {
+    setPasswordUpdated(true);
+  };
+
+  return !passwordUpdated ? (
     <aside className={`${modal.base} ${modal.member}`}>
       <h2>비밀번호 찾기</h2>
-      <PasswordType style={{ marginBottom: "16px" }} />
-      <Primary
-        onClick={() => setShowNewPassword(true)}
-        text="확인" // 하나라도 아니면 경고창 발생
-        //아이디, 이름, 비번찾기 질문이 다 맞을때 비밀번호 수정 모달창으로 이동
-        style={{ marginBottom: "64px" }}
+      <PasswordType
+        style={{ marginBottom: "16px" }}
+        setShowNewPassword={setShowNewPassword}
       />
       <div className={modal.extraButtons}></div>
       <button
@@ -27,10 +28,13 @@ function PasswordModal({ onClick }) {
         <GrClose />
       </button>
       {showNewPassword && (
-        <NewPasswordModal onClick={() => setShowNewPassword(false)} />
+        <NewPasswordModal
+          onClick={() => setShowNewPassword(false)}
+          onCloseModal={handlePasswordUpdated}
+        />
       )}
     </aside>
-  );
+  ) : null;
 }
 
 export default PasswordModal;

c9d4ebb6b95f69b0778767b6f2a9edbbde8243d4
TW <my1268@naver.com>
2023-08-22 16:44:34 +0900

    axios 추가

--- src/UI/Form/PasswordType.js
+++ src/UI/Form/PasswordType.js
@@ -1,11 +1,18 @@
 import React, { useState } from "react";
 import form from "./Form.module.css";
 import { VscTriangleDown } from "react-icons/vsc";
+import Primary from "../Button/Primary";
+import axios from "axios";
 
-function PasswordType({ style }) {
+function PasswordType({ style, setShowNewPassword }) {
   const [showOptions, setShowOptions] = useState(false);
   const [selectQuestion, setSelectQuestion] = useState("");
   const [showAddInput, setShowAddInput] = useState(false);
+
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [addInput, setAddInput] = useState("");
+
   const OptionsClick = () => {
     setShowOptions(!showOptions);
   };
@@ -18,17 +25,45 @@ function PasswordType({ style }) {
     setShowAddInput(true);
   };
 
+  const handleConfirm = async () => {
+    const data = {
+      email,
+      name,
+      question: selectQuestion,
+      addInput,
+    };
+    try {
+      const response = await axios.post("/api/check-identity", data, {
+        //예시 URL
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      if (response.ok) {
+        //예시 response
+        setShowNewPassword(true);
+      } else {
+        alert("다시 확인해주세요");
+      }
+    } catch (error) {
+      console.error("Error checking identity:", error);
+    }
+  };
   return (
     <div style={style} className={form.loginType}>
       <input
         className={`${form.base} ${form.inputMargin}`}
         type="text"
         placeholder="이메일"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
       />
       <input
         className={`${form.base} ${form.inputMargin}`}
         type="text"
         placeholder="이름"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
       />
       <div className={form.findPassword}>
         <div className={form.inputMargin} onClick={OptionsClick}>
@@ -59,9 +94,16 @@ function PasswordType({ style }) {
             className={form.findPasswordValue}
             type="text"
             placeholder={`여기에 ${selectQuestion}에 대한 값을 적어주세요`}
+            value={addInput}
+            onChange={(e) => setAddInput(e.target.value)}
           />
         </div>
       )}
+      <Primary
+        onClick={handleConfirm}
+        text="확인"
+        style={{ marginBottom: "64px", marginTop: "16px" }}
+      />
     </div>
   );
 }
--- src/UI/Modal/NewPasswordModal.js
+++ src/UI/Modal/NewPasswordModal.js
@@ -3,8 +3,9 @@ import modal from "./Modal.module.css";
 import Primary from "../Button/Primary";
 import { GrClose } from "react-icons/gr";
 import NewPasswordType from "../Form/NewPasswordType";
+import axios from "axios";
 
-function NewPasswordModal({ onClick }) {
+function NewPasswordModal({ onClick, onCloseModal }) {
   const [newPassword, setNewPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [passwordError, setNewPasswordError] = useState("");
@@ -29,14 +30,33 @@ function NewPasswordModal({ onClick }) {
 
   const match = newPassword === confirmPassword;
 
-  const handleSubmit = () => {
+  const handleSubmit = async () => {
     if (!validatePassword() || !match) {
       return;
     } else {
-      alert("비밀번호 수정완료"); //비밀번호 수정 post
+      try {
+        const response = await axios.post(
+          "/api/update-password", // 예시 URL
+          { newPassword, confirmPassword },
+          {
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+        if (response.data.success) {
+          // 예시 response
+          alert("비밀번호 수정이 완료되었습니다.");
+          onCloseModal();
+        } else {
+          alert("비밀번호 수정에 실패했습니다. 다시 시도해주세요.");
+        }
+      } catch (error) {
+        console.error("Error updating password:", error);
+        alert("비밀번호 수정에 실패했습니다. 다시 시도해주세요.");
+      }
     }
   };
-
   return (
     <aside className={`${modal.base} ${modal.member}`}>
       <h2>비밀번호 수정</h2>
@@ -48,13 +68,12 @@ function NewPasswordModal({ onClick }) {
       <Primary
         text="저장"
         style={{ marginBottom: "64px" }}
-        onClick={handleSubmit} // 수정 비번  post
+        onClick={handleSubmit}
       />
       <p style={{ color: "red" }}>{passwordError}</p>
       {newPassword !== confirmPassword && (
         <p style={{ color: "red" }}>비밀번호가 일치하지 않습니다.</p>
       )}
-
       <button
         type="button"
         className={`sm-only ${modal.close}`}

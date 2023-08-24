9a02d3b53e500113d61bbf69a6d2b1566fdea7a6
TW <my1268@naver.com>
2023-08-25 00:26:05 +0900

    onClick 추가

--- src/UI/Card/Card.js
+++ src/UI/Card/Card.js
@@ -1,11 +1,11 @@
 import React from "react";
 import card from "./Card.module.css";
 
-function Card({ place }) {
+function Card({ place, onClick }) {
   return (
     <li className={card.item} key={place.name}>
       <div className={card.image}>
-        <img src={place.image} alt={place.name} />
+        <img src={place.image} alt={place.name} onClick={onClick} />
       </div>
       <h4 className={card.inquiry}>{place.inquiry}</h4>
       <h4 className={card.name}>{place.name}</h4>
--- src/UI/Card/CardList.js
+++ src/UI/Card/CardList.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import card from './Card.module.css';
-import Card from './Card';
+import React from "react";
+import card from "./Card.module.css";
+import Card from "./Card";
 
-function CardList({ placeList }) {
+function CardList({ placeList, onClick }) {
   return (
     <ul className={card.list}>
       {placeList.map((place) => {
-        return <Card place={place} />;
+        return <Card place={place} onClick={onClick} />;
       })}
     </ul>
   );

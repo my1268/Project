ba940313a61b7fba0e8b02593898d07aa12d5a31
TW <my1268@naver.com>
2023-08-28 20:27:21 +0900

    value 추가

--- src/UI/Form/Base.js
+++ src/UI/Form/Base.js
@@ -1,13 +1,14 @@
 import React from "react";
 import form from "./Form.module.css";
 
-function Base({ type = "text", placeholder, onChange, maxLength }) {
+function Base({ type = "text", placeholder, onChange, maxLength, value }) {
   return (
     <input
       className={form.base}
       type={type}
       placeholder={placeholder}
       onChange={onChange}
+      value={value}
       maxLength={maxLength}
     />
   );

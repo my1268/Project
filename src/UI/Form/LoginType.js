5fb3e010a7389823b1098eb9473be556cc091022
TW <my1268@naver.com>
2023-08-30 18:06:56 +0900

    로그인 타입.js

--- src/UI/Form/LoginType.js
+++ src/UI/Form/LoginType.js
@@ -1,11 +1,23 @@
 import React from "react";
 import form from "./Form.module.css";
 
-function LoginType({ style }) {
+function LoginType({ style, email, password, setEmail, setPassword }) {
   return (
     <div style={style} className={form.loginType}>
-      <input className={form.base} type="text" placeholder="이메일" />
-      <input className={form.base} type="password" placeholder="비밀번호" />
+      <input
+        className={form.base}
+        type="text"
+        placeholder="이메일"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        className={form.base}
+        type="password"
+        placeholder="비밀번호"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
     </div>
   );
 }

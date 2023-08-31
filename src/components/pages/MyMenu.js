54c93ed6f5fd20c5a5599c13d510b78d7a7c929b
TW <my1268@naver.com>
2023-08-31 19:28:27 +0900

    HTTP 프로토콜 추가

--- src/components/pages/MyMenu.js
+++ src/components/pages/MyMenu.js
@@ -1,15 +1,100 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Categories from "../features/Categories";
 import PageCover from "../features/PageCover";
 import myMenu from "./MyMenu.module.css";
 import Base from "../../UI/Form/Base";
 import Primary from "../../UI/Button/Primary";
+import ButtonType from "../../UI/Form/ButtonType";
+import axios from "axios";
 
 function MyMenu() {
+  const [nickName, setNickName] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
 
+  const [loading, setLoading] = useState(true);
+  const [nickNameError, setNickNameError] = useState("");
+  const [nickNameChecked, setNickNameChecked] = useState(false);
   const [confirmPasswordError, setConfirmPasswordError] = useState("");
+
+  useEffect(() => {
+    async function getNickName() {
+      try {
+        const response = await axios.get("/member/info");
+        if (response.data.success) {
+          setNickName(response.data.nickname);
+        } else {
+          console.error("Failed get nickname:", response.data.errorMessage);
+        }
+      } catch (error) {
+        console.error("Error get nickname:", error);
+      } finally {
+        setLoading(false);
+      }
+    }
+    getNickName();
+  }, []);
+
+  const validateNickName = () => {
+    const NickNameRegex = /^.{2,12}$/;
+    if (!nickName.match(NickNameRegex)) {
+      setNickNameError("닉네임을 수정하려면 2~12자 사이여야 합니다.");
+      return false;
+    }
+    return true;
+  };
+
+  const handleNicknameCheck = async () => {
+    if (!nickName) {
+      alert("닉네임을 먼저 입력해주세요.");
+      return;
+    }
+    try {
+      const response = await axios.post(
+        "/api/check-nickname", // 예시 URL
+        { nickName },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      if (response.data.exists) {
+        alert("이미 사용 중인 닉네임입니다.");
+      } else {
+        alert("사용 가능한 닉네임입니다.");
+        setNickNameChecked(true);
+        setNickNameError("");
+      }
+    } catch (error) {
+      console.error("Error checking nickname:", error);
+    }
+  };
+
+  async function putUpdateNickname() {
+    if (!validateNickName()) {
+      return;
+    }
+    if (!nickNameChecked) {
+      alert("닉네임 중복 확인을 먼저 해주세요.");
+      return;
+    }
+    const confirmUpdate = window.confirm("정말로 변경하시겠습니까?");
+    if (!confirmUpdate) {
+      return;
+    }
+    try {
+      const response = await axios.put("/member/update", { nickName });
+      if (response.data.success) {
+        console.log("Successed nickname update.");
+        alert("닉네임이 성공적으로 변경되었습니다.");
+      } else {
+        console.error("Failed nickname update.", response.data.errorMessage);
+      }
+    } catch (error) {
+      console.error("Error put nickname:", error);
+    }
+  }
 
   const validateConfirmPassword = () => {
     const changePasswordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,20}$/;
@@ -29,13 +114,31 @@ function MyMenu() {
     setConfirmPassword(event.target.value);
     setConfirmPasswordError("");
   };
+
   const match = password === confirmPassword;
 
-  const handleSubmit = () => {
+  const handleSubmit = async () => {
     if (!validateConfirmPassword() || !match) {
       return;
-    } else {
-      alert("회원 탈퇴 완료"); //회원 탈퇴 post
+    }
+    const confirmDelete = window.confirm("정말로 탈퇴하시겠습니까?");
+    if (!confirmDelete) {
+      return;
+    }
+    try {
+      const response = await axios.delete("/member/withdraw", {
+        data: {
+          password: password,
+          confirmPassword: confirmPassword,
+        },
+      });
+      if (response.data.success) {
+        alert("회원 탈퇴가 완료되었습니다.");
+      } else {
+        console.error("Failed to delete member:", response.data.errorMessage);
+      }
+    } catch (error) {
+      console.error("Error deleting member:", error);
     }
   };
   return (
@@ -49,11 +152,17 @@ function MyMenu() {
               <div className={myMenu.item}>
                 <dt>닉네임</dt>
                 <dd>
-                  <Base placeholder="닉네임" />
+                  <ButtonType
+                    placeholder="닉네임"
+                    value={loading ? "로딩 중..." : nickName}
+                    onChange={(e) => setNickName(e.target.value)}
+                    onClick={handleNicknameCheck}
+                  />
                 </dd>
               </div>
             </dl>
-            <Primary text="닉네임 수정" />
+            <Primary onClick={putUpdateNickname} text="닉네임 수정" />
+            <p style={{ color: "red" }}>{nickNameError}</p>
           </form>
           <form
             style={{ position: "relative", top: "90px" }}

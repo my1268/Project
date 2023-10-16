0b62a0a6caa166e4ed56da59f7f8b23645a99fbc
TW <my1268@naver.com>
2023-10-17 03:21:42 +0900

    코드 수정

--- src/UI/TimeTable/TimeTable.js
+++ src/UI/TimeTable/TimeTable.js
@@ -2,34 +2,63 @@ import React from "react";
 import table from "./TimeTable.module.css";
 
 function TimeTable({ savePlaceData }) {
-  const renderTimeTable = () => {
-    console.log(savePlaceData);
-    if (!savePlaceData || !savePlaceData.length) {
-      return <p>장소 데이터가 없습니다.</p>;
-    }
-    return savePlaceData.map((item, index) => (
-      <li key={index}>
-        <h4 className={table.header}>{item.date}</h4>
-        <ul className={table.contents}>
-          <li>{item.place}</li>
-        </ul>
-      </li>
-    ));
+  const formatDate = (dateArray) => {
+    return dateArray.join("-");
   };
+
+  const formatTime = (timeArray) => {
+    return timeArray
+      .slice(3)
+      .map((item) => item.toString().padStart(2, "0"))
+      .join(":");
+  };
+
   return (
     <div className={table.table}>
-      <div className={table.time}>
-        <h4 className={table.header}>시간/날짜</h4>
-        <ul className={table.contents}>
-          <li>06:00-09:00</li>
-          <li>09:00-12:00</li>
-          <li>12:00-15:00</li>
-          <li>15:00-18:00</li>
-          <li>18:00-21:00</li>
-          <li>21:00-24:00</li>
-        </ul>
-      </div>
-      {renderTimeTable()}
+      {savePlaceData && savePlaceData.length ? (
+        <div className={table.row}>
+          <div className={table.cell}>
+            <h4 className={table.header}>날짜</h4>
+          </div>
+          <div className={table.cell}>
+            <h4 className={table.header}>장소</h4>
+          </div>
+          <div className={table.cell}>
+            <h4 className={table.header}>출발 시간</h4>
+          </div>
+          <div className={table.cell}>
+            <h4 className={table.header}>도착 시간</h4>
+          </div>
+        </div>
+      ) : (
+        <p>장소 데이터가 없습니다.</p>
+      )}
+      {savePlaceData &&
+        savePlaceData.length &&
+        savePlaceData.map((item, index) => (
+          <div className={table.row} key={index}>
+            <div className={table.cell}>
+              <ul className={table.contents}>
+                <li>{formatDate(item.date)}</li>
+              </ul>
+            </div>
+            <div className={table.cell}>
+              <ul className={table.contents}>
+                <li>{item.place}</li>
+              </ul>
+            </div>
+            <div className={table.cell}>
+              <ul className={table.contents}>
+                <li>{formatTime(item.startTime)}</li>
+              </ul>
+            </div>
+            <div className={table.cell}>
+              <ul className={table.contents}>
+                <li>{formatTime(item.arriveTime)}</li>
+              </ul>
+            </div>
+          </div>
+        ))}
     </div>
   );
 }
